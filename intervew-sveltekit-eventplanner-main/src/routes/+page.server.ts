1cef493c0733100c7014e1244c4a880153223b51
Daniel Markovitch <ds.markovitch@gmail.com>
2026-05-02 12:18:00 -0400

    Freestyle - sort events by date ascending and add confirm dialog before delete

--- intervew-sveltekit-eventplanner-main/src/routes/+page.server.ts
+++ intervew-sveltekit-eventplanner-main/src/routes/+page.server.ts
@@ -3,6 +3,8 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = () => {
     return {
-        events: fetchAllEvents()
+        events: fetchAllEvents().then(events => 
+            events.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+        )
     }
 };
